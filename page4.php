ca28c96876df03133a2936f023605df848941801
Robert Thomas <FireSqwall@Gmail.com>
2017-07-04 21:13:05 -0400

    Add files via upload
    Adding the img folder and our example Top 5.

---
+++ page4.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+
+       <!-- Begin page content -->
+    <div class="container">
+    <div class="row">
+  <div class="col-sm-8">
+      <br>
+
+        <h1>3. Ted Sheckler</h1>
+    
+      <BR>
+        <img src="img/batting-10-1557105-639x426.jpg" alt="Ted Sheckler" class="img-fluid">
+      <BR><BR>
+     <p>Choro nostro ancillae ad vis. An pri nisl impetus vituperata, has at aperiri postulant. Minim causae pro no, est purto habemus an, cum viderer fastidii salutandi ea. Pri utamur inimicus id, cum ei minimum consulatu vituperata.Mutat ubique nam ex, utinam aliquip pri et. Bonorum atomorum usu ex, id libris splendide sed. Ex eam fugit mandamus. Mei te stet dolores signiferumque. Pro sententiae argumentum an, ex usu summo facilis. Et eros doming est, odio altera in qui.</p>
+  <BR>
+  <p>Mel an erat fabulas appareat, in mel dolorem scaevola. Mea illud voluptaria te, mel te unum mucius vivendo. Cum cu tamquam elaboraret, ad legere corpora sea. Tale zril usu in, adhuc eruditi mel ut. Idque viderer admodum ut nec, omnes bonorum id eam. Ea dolore audire euripidis vix, eu vel sonet errem vocibus.
+  Phaedrum platonem concludaturque ut vel, te aeterno utroque sed. Utamur labitur volutpat pri ea. Pri rebum legere ne, ea ius libris labore. Ius lorem feugait ut. Has purto detracto at. </p>
+        <center>
+      <a href="page5.php"><button type="button" class="btn btn-primary btn-lg"> NEXT! </button></a>
+
+    </div>
+     <div class="col-sm-4">
+      <BR><BR>
+<?php include 'sidestuff.php'; ?>
---
+++ page7.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+
+       <!-- Begin page content -->
+    <div class="container">
+    <div class="row">
+  <div class="col-sm-8">
+      <br>
+
+        <h1>Ending Page</h1>
+    
+      <BR>
+        <img src="img/oakland-baseball-1378656-639x511.jpg" alt="Ending" class="img-fluid">
+      <BR><BR>
+     <p>Choro nostro ancillae ad vis. An pri nisl impetus vituperata, has at aperiri postulant. Minim causae pro no, est purto habemus an, cum viderer fastidii salutandi ea. Pri utamur inimicus id, cum ei minimum consulatu vituperata.Mutat ubique nam ex, utinam aliquip pri et. Bonorum atomorum usu ex, id libris splendide sed. Ex eam fugit mandamus. Mei te stet dolores signiferumque. Pro sententiae argumentum an, ex usu summo facilis. Et eros doming est, odio altera in qui.</p>
+  <BR>
+  <p>Mel an erat fabulas appareat, in mel dolorem scaevola. Mea illud voluptaria te, mel te unum mucius vivendo. Cum cu tamquam elaboraret, ad legere corpora sea. Tale zril usu in, adhuc eruditi mel ut. Idque viderer admodum ut nec, omnes bonorum id eam. Ea dolore audire euripidis vix, eu vel sonet errem vocibus.
+  Phaedrum platonem concludaturque ut vel, te aeterno utroque sed. Utamur labitur volutpat pri ea. Pri rebum legere ne, ea ius libris labore. Ius lorem feugait ut. Has purto detracto at. </p>
+        <center>
+      <a href="index.php"><button type="button" class="btn btn-primary btn-lg"> Head back! </button></a>
+
+    </div>
+     <div class="col-sm-4">
+      <BR><BR>
+<?php include 'sidestuff.php'; ?>
